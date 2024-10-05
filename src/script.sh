2b7c3e0e2523e528532536053fef28505d0beea9
Valeria Zaborovskaia <VViZaborovskaya@sberbank.ru>
2024-10-06 01:13:26 +0300

    first commit

---
+++ src/script.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+sleep_time=60 # интервал мониторинга в секундах
+
+function start_monitoring {
+    local timestamp=$(date +"%Y%m%d_%H%M%S")
+    local log_file="monitor_${timestamp}.csv"
+    touch log_file
+
+    # запуск мониторинга
+    (
+        while true; do
+            local current_date=$(date +"%Y-%m-%d")
+            local disk_usage=$(df / | awk 'NR==2 {print $5}')
+            local free_inodes=$(df -i / | awk 'NR==2 {print $4}')
+#            touch log_file
+            echo "$(date +"%Y-%m-%d %H:%M:%S"),${disk_usage},${free_inodes}" >> "${log_file}"
+
+            # обновление лог-файла при переходе на новый день
+            if [[ $(date +"%Y-%m-%d") != "$current_date" ]]; then
+                timestamp=$(date +"%Y%m%d_%H%M%S")
+                log_file="monitor_${timestamp}.csv"
+                rm -f
+            fi
+
+            sleep sleep_time
+        done
+    ) &
+    echo $! > monitor.pid
+    echo "Monitoring started with PID $!"
+}
+
+function stop_monitoring {
+    if [[ -f monitor.pid ]]; then
+        local pid=$(cat monitor.pid)
+        if kill -0 $pid > /dev/null 2>&1; then
+            kill $pid
+            rm -f monitor.pid
+            echo "Monitoring stopped."
+        else
+            echo "No running process found."
+        fi
+    else
+        echo "No PID file found. Monitoring may not be running."
+    fi
+}
+
+function status_monitoring {
+    if [[ -f monitor.pid ]]; then
+        local pid=$(cat monitor.pid)
+        if kill -0 $pid > /dev/null 2>&1; then
+            echo "Monitoring is running with PID $pid."
+        else
+            echo "Monitoring is not running."
+        fi
+    else
+        echo "Monitoring is not running."
+    fi
+}
+
+case "$1" in
+    START)
+        start_monitoring
+        ;;
+    STOP)
+        stop_monitoring
+        ;;
+    STATUS)
+        status_monitoring
+        ;;
+    *)
+        echo "Usage: $0 {START|STOP|STATUS}"
+        ;;
+esac
